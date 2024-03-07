65d93d6ed5b1d3db8300773062f6f7ea16a922d7
HP <sibgatullah>
2024-03-07 11:36:36 +0600

    second

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,15 +1,20 @@
 qr = str => document.querySelector(str)
 
 let search = qr(".search");
+let suggestion = qr(".suggestion")
 let searchInput = qr(".search_bar input");
+let overlay = qr(".overlay");
 
-searchInput.addEventListener('click', (e) => {
-    console.log(e)
+searchInput.addEventListener('click', () => {
+    search.classList.add('active');
+    suggestion.style.display = "block";
+    overlay.style.display = "block";
 })
-console.log(searchInput)
+overlay.addEventListener('click', (e) => {
+    console.log("overlay")
+    search.classList.remove('active');
+    suggestion.style.display = "none";
+    overlay.style.display = "none";
+})
 
-// searchInput.addEventListener('change', (e) => {
-//     console.log(e)
-//     search.classList.remove('active')
 
-// })
